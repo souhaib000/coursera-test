835188d9d8febd460f2ef318d2211c4491fc199c
Your Name <you@example.com>
2024-02-27 23:56:21 +0100

    example for site lion

---
+++ Lecture40/js/script.js
@@ -0,0 +1,15 @@
+// var names=["yaakov","john"];
+//  var myobj={
+// name:"yaakov",
+// course:"html/css",
+// platform:"coursera",
+//  };
+// for (var prop in myobj){
+//    console.log(prop + ":" + myobj[prop]);
+// }
+var counter = 0;
+var myArray = ["Yaakov", 2, {handle: "yaakovchaikin"}];
+for (var i = 0; i < myArray.length; i++) {
+  counter++;
+}
+console.log(counter);
